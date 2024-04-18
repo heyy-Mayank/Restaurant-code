3453eb9d77506248875ae426655abdfef7f1e4f5
Mayank <kashyapmayank0612@gmail.com>
2024-04-18 22:26:48 +0530

    First Commit

---
+++ java.js
@@ -0,0 +1,37 @@
+$(document).ready(function () {
+    
+    $("a").on("click", function (event) {
+        
+        if (this.hash !== "") {
+            
+            event.preventDefault();
+           
+            var hash = this.hash;
+            
+            $("html, body").animate(
+                {
+                    scrollTop: $(hash).offset().top,
+                },
+                800,
+                function () {
+                    window.location.hash = hash;
+                }
+            );
+        } 
+    });
+});
+const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
+
+let lastScrollPos = 0;
+
+const hideHeader = function () {
+  const isScrollBottom = lastScrollPos < window.scrollY;
+  if (isScrollBottom) {
+    header.classList.add("hide");
+  } else {
+    header.classList.remove("hide");
+  }
+
+  lastScrollPos = window.scrollY;
+}
